b13e62ad07b13cde70cac0aab7212d820febbab3
goescat <goescat1024@gmail.com>
2019-03-19 10:43:12 +0800

    Sort all json file from folder

--- json_sort_and_classification.py
+++ json_sort_and_classification.py
@@ -6,37 +6,53 @@ import fileinput
 from collections import OrderedDict
 from itertools import chain
 
+##Get file path from cmd argument
 file_path = sys.argv[1]
 
-with open(file_path, 'rb') as f:
-    raw_data = f.read()
-    dict_ = json.loads(raw_data.decode('utf-8'))
+##Create file list for add .json file
+file_list = []
 
-testcase_output = {}
-other_output = {}
+for root, dirs, files in os.walk(file_path):  
+    for filename in files:
+        if ".json" in filename:
+            file_list.append(filename)
 
-for key, value in dict_.iteritems():
-    # if a character in string.
-    if "-" in key:
-        testcase_output[key] = value
-        # print output
-    if "-" not in key:
-        other_output[key] = value
-        # print output
+##Sort file from file list
+for filename in file_list:
+    filepath_and_filename = file_path + "\\" + filename
 
-testcase_output_json = json.dumps(testcase_output, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
-other_output_json = json.dumps(other_output, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
+    ##Open file with binary
+    with open(filepath_and_filename, 'rb') as f:
+        raw_data = f.read()
+        ##decode with utf-8 and user json.loads to dictionary
+        dict_ = json.loads(raw_data.decode('utf-8'))
 
-testcase_output_dict = json.loads(testcase_output_json, object_pairs_hook=OrderedDict)
-other_output_dict = json.loads(other_output_json, object_pairs_hook=OrderedDict)
+    ##Create testcase and other method dictionary
+    testcase_output = {}
+    other_output = {}
 
-output = OrderedDict(chain(testcase_output_dict.items(), other_output_dict.items()))
-# print output
+    for key, value in dict_.iteritems():
+        if "-" in key:
+            ##save in testcase_output dict
+            testcase_output[key] = value
+        elif "-" not in key:
+            ##save in other_output dict
+            other_output[key] = value
 
-output_json = json.dumps(output, indent=4, separators=(', ', ': '), ensure_ascii=False)
+    ##Use json.dumps to sort dict and to json
+    testcase_output_json = json.dumps(testcase_output, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
+    other_output_json = json.dumps(other_output, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
 
-# print output
-file_name = os.path.basename(file_path)
+    ##Use json.loads to ordered dictionary to keep json order
+    testcase_output_dict = json.loads(testcase_output_json, object_pairs_hook=OrderedDict)
+    other_output_dict = json.loads(other_output_json, object_pairs_hook=OrderedDict)
 
-with open(file_name, 'ab') as f:
-    f.write(output_json.encode('utf-8'))
+    ##Merge 2 ordered dictionary
+    output = OrderedDict(chain(testcase_output_dict.items(), other_output_dict.items()))
+
+    ##Use json.dumps to json
+    output_json = json.dumps(output, indent=4, separators=(', ', ': '), ensure_ascii=False)
+
+    ##Write file
+    with open(filename, 'ab') as f:
+        f.write(output_json.encode('utf-8'))
